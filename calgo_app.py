2f3238d1b1ffad4b32b65de31a28d88e7177992c
KartikKapur <kartik@airbears2-10-142-34-211.airbears2.1918.berkeley.edu>
2016-11-17 13:50:02 -0800

    mod

--- calgo_app.py
+++ calgo_app.py
@@ -1,8 +1,20 @@
-import os
+
 from flask import Flask
+from flask import request, redirect
+import json
+import requests
+import urllib
+from webob import Response
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        if request.args.get('hub.verify_token') == 'bear':
+            return request.args.get('hub.challenge')
+        else:
+            return 'Wrong validation token'
+
+    return Response()
