ce4fa87adda6868328dca1bbd839725ac14bfbd1
KartikKapur <kartik@airbears2-10-142-168-55.airbears2.1918.berkeley.edu>
2016-11-17 21:35:26 -0800

    ye

--- calgo_app.py
+++ calgo_app.py
@@ -1,126 +1,86 @@
-from flask import Flask
-from flask import request, redirect
-import json
-import requests
-import urllib
-from webob import Response
 import os
 import sys
-from pymongo import MongoClient
+import json
 
-
-
-FB_APP_TOKEN = 'EAAPmvnm2ZAaUBADCZCBCQL9tMkf7tLyVJxdezJeKPjwOa1uYsgcaK0eZCFiVOcoGhms5M7580ZCCXHAuZBczzdOmaeEjLgnMCoiyM9BwxmZCuqsXlSlmeOW8DnfLDq0CK23HTxBv44QGAjQYPUPBC1yRql66J2S80YEjSU7QZBHGAZDZD'
-FB_ENDPOINT = 'https://graph.facebook.com/v2.6/me/{0}'
-FB_MESSAGES_ENDPOINT = FB_ENDPOINT.format('messages')
-FB_THREAD_SETTINGS_ENDPOINT = FB_ENDPOINT.format('thread_settings')
-# MONGO_DB_BEARMAX_DATABASE = ' '
-# MONGO_DB_BEARMAX_ENDPOINT = ' '
-# MONGO_DB_BEARMAX_PORT = ''
-# MONGO_DB_USERNAME = ' '
-# MONGO_DB_PASSWORD = ' '
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
-def connect():
-    connection = MongoClient(
-    #     MONGO_DB_BEARMAX_ENDPOINT,
-    #     MONGO_DB_BEARMAX_PORT
-    # )
-    # handle = connection[MONGO_DB_BEARMAX_DATABASE]
-    # handle.authenticate(
-    #     MONGO_DB_USERNAME,
-    #     MONGO_DB_PASSWORD
-    )
-    return handle
-
-app = Flask(__name__)
-app.config['DEBUG'] = True
-handle = connect()
 
 
-@app.route('/')
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
 def webhook():
-    if request.method == 'GET':
-        if request.args.get('hub.verify_token') == 'calgo':
-            return request.args.get('hub.challenge')
-        else:
-            return 'Wrong validation token'
-    elif request.method =='POST':
-        data = json.loads(request.data)['entry'][0]['messaging']
-        for i in range(len(data)):
-            event = data[i]
-            if 'sender' in event:
-                print('Event: {0}'.format(event))
-                sender_id = event['sender']['id']
-                if 'message' in event and 'is_echo' in event['message'] and event['message']['is_echo']:
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    send_message(sender_id, "got it, thanks!")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
                     pass
-                else:
-                    send_FB_text(sender_id, 'Hello! I am Bearmax, your personal healthcare companion.')
-                    init_bot_user(sender_id)
 
-    return Response()
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
 
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
 
-def send_FB_message(sender_id, message):
-    fb_response = requests.post(
-        FB_MESSAGES_ENDPOINT,
-        params={'access_token': FB_APP_TOKEN},
-        data=json.dumps(
-            {
-                'recipient': {
-                    'id': sender_id
-                },
-                'message': message
-            }
-        ),
-        headers={'content-type': 'application/json'}
-    )
-    if not fb_response.ok:
-        app.logger.warning('Not OK: {0}: {1}'.format(
-            fb_response.status_code,
-            fb_response.text))
-
-def send_FB_text(sender_id, text, quick_replies=None):
-    message = {'text': text}
-    if quick_replies:
-        message['quick_replies'] = quick_replies
-    return send_FB_message(
-        sender_id,
-        message)
+    return "ok", 200
 
 
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 
-def send_FB_buttons(sender_id, text, buttons):
-    return send_FB_message(
-        sender_id,
-        {
-            'attachment': {
-                'type': 'template',
-                'payload': {
-                    'template_type': 'button',
-                    'text': text,
-                    'buttons': buttons
-                }}})
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print(str(message))
+    sys.stdout.flush()
 
-def init_bot_user(sender_id):
-    send_FB_text(
-        sender_id,
-        'Do you want to view your events or make one?',
-        quick_replies=[
-            {
-                'content_type': 'text',
-                'title': 'View',
-                'payload': ''
-            },
-            {
-                'content_type': 'text',
-                'title': 'Make',
-                'payload': 'Gender:make'
-            }
-        ]
-    )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
