1e8611bfb942aeb5159b9993eda6d247cf84fa67
KartikKapur <kartik@Sandeeps-MacBook-Pro.local>
2016-11-19 15:47:41 -0800

    yey

--- calgo_app.py
+++ calgo_app.py
@@ -6,6 +6,7 @@ import urllib
 from webob import Response
 from pymongo import MongoClient
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 FB_APP_TOKEN= 'EAAPmvnm2ZAaUBAG852TcAX8FTZCwgeAiHLr6E8VZBniT8OUuLcKaBCZBMLiMxHNh5zQgKx7eOWMTCPyOGnJizZBi2qgyCwTxgqoY4nQeT6ljSFblqxvAyofd0VnY4iv296jIUGDZA7NlihX0h4pNeEvl57m2vrAVBnewCvi9l69AZDZD'
 FB_ENDPOINT = 'https://graph.facebook.com/v2.6/me/{0}'
@@ -33,13 +34,17 @@ def connect():
     )
     return handle
 
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 handle = connect()
 
+
+@app.route('/')
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'GET':
-        if request.args.get('hub.verify_token') == 'bear':
+        if request.args.get('hub.verify_token') == 'calgo':
             return request.args.get('hub.challenge')
         else:
             return 'Wrong validation token'
@@ -52,9 +57,9 @@ def webhook():
                 sender_id = event['sender']['id']
                 if 'message' in event and 'is_echo' in event['message'] and event['message']['is_echo']:
                     pass
-                elif handle.bot_users.find({'sender_id': sender_id}).count() == 0:
-                    send_FB_text(sender_id, 'Hello! I am Bearmax, your personal healthcare companion.')
-    return Response
+                else:
+                    send_FB_text(sender_id, 'What can I do for you?')
+    return Response()
 
 
 def send_FB_text(sender_id, text, quick_replies=None):
