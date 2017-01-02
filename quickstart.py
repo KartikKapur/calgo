f31741076d8b7591edf5e140564c89ecc95ddba0
KartikKapur <kartik@Sandeeps-MacBook-Pro.local>
2017-01-01 21:31:27 -0800

    trying again

--- quickstart.py
+++ quickstart.py
@@ -11,7 +11,7 @@ import datetime
 
 try:
     import argparse
-    # flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
 
@@ -43,8 +43,8 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        # if flags:
-        #     credentials = tools.run_flow(flow, store, flags)
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
         # else: # Needed only for compatibility with Python 2.6
         #     credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
