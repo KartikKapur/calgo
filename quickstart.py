b6eda90359753c758b192aca6068d9d7e17610a4
KartikKapur <kartik@Sandeeps-MacBook-Pro.local>
2017-01-01 23:45:00 -0800

    modified so would keep facebook bot open

--- quickstart.py
+++ quickstart.py
@@ -35,8 +35,7 @@ def get_credentials():
     # credential_dir = os.path.join(home_dir, '.credentials')
     # if not os.path.exists(credential_dir):
     #     os.makedirs(credential_dir)
-    credential_path = os.path.join(
-                                   'calgocredential.json')
+    credential_path = os.path.join('calgocredential.json')
 
     store = Storage(credential_path)
     credentials = store.get()
