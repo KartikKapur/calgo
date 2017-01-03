87519e24eaddde8d9424695d29d02981d0756511
KartikKapur <kartik@Sandeeps-MacBook-Pro.local>
2017-01-02 21:44:11 -0800

    changed a bit

--- quickstart.py
+++ quickstart.py
@@ -6,6 +6,9 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+from oauth2client.client import GoogleCredentials
+from googleapiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
 
 import datetime
 
@@ -17,10 +20,9 @@ except ImportError:
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = './secret1.json'
 APPLICATION_NAME = 'calgo'
-
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -32,13 +34,14 @@ def get_credentials():
         Credentials, the obtained credential.
     """
     home_dir = os.path.expanduser('./')
-    # credential_dir = os.path.join(home_dir, '.credentials')
-    # if not os.path.exists(credential_dir):
-    #     os.makedirs(credential_dir)
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
     credential_path = os.path.join('calgocredential.json')
 
     store = Storage(credential_path)
     credentials = store.get()
+    # credentials = GoogleCredentials.get_application_default()
     print('Storing credentials to ' + credential_path)
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
