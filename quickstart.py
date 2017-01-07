9de081b9bd3f12720cee19142d585164417c8395
KartikKapur <kartik@Sandeeps-MacBook-Pro.local>
2017-01-06 18:25:33 -0800

    try

--- quickstart.py
+++ quickstart.py
@@ -13,11 +13,11 @@ from googleapiclient.discovery import build
 
 import datetime
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+# try:
+#     import argparse
+#     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+# except ImportError:
+#     flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/calendar-python-quickstart.json
@@ -46,12 +46,12 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-            print('step4')
+        # if flags:
+        #     credentials = tools.run_flow(flow, store, flags)
+        #     print('step4')
 
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        # Needed only for compatibility with Python 2.6
+        credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
